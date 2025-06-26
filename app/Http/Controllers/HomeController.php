9e612ee41951339e6220e639b566a946fb0b106f
RanggaCasper <hyfanutama@gmail.com>
2025-06-26 12:56:31 +0800

    restaurant review home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -18,7 +18,7 @@ class HomeController extends Controller
     public function index()
     {
         $comment = Review::with('restaurant')
-            ->latest()
+            ->latest('id')
             ->take(6)
             ->get();
 
